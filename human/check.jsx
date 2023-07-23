3914ff1a5ff29142437a64ca1156feb66cef7e5c
Alaap Joshi <alaapjoshi@gmail.com>
2023-07-23 14:59:48 -0400

    forgot to save and leaked api key lol

--- human/check.jsx
+++ human/check.jsx
@@ -13,12 +13,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0cC4E5Ue8RcXZEYmE871YehcrfQkLDT8",
-    authDomain: "glob-ed-2815e.firebaseapp.com",
-    projectId: "glob-ed-2815e",
-    storageBucket: "glob-ed-2815e.appspot.com",
-    messagingSenderId: "596120472029",
-    appId: "1:596120472029:web:f16d07c5ae810b79510e34"
+  "YOU NOT GETTING ANYTHING"
   };
   const app = initializeApp(firebaseConfig);
 
--- human/signup.jsx
+++ human/signup.jsx
@@ -7,12 +7,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0cC4E5Ue8RcXZEYmE871YehcrfQkLDT8",
-  authDomain: "glob-ed-2815e.firebaseapp.com",
-  projectId: "glob-ed-2815e",
-  storageBucket: "glob-ed-2815e.appspot.com",
-  messagingSenderId: "596120472029",
-  appId: "1:596120472029:web:f16d07c5ae810b79510e34"
+  "YOU NOT GETTING ANYTHING"
 };
 
 // Initialize Firebase
